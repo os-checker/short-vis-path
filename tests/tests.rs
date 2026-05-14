b1308e67d79e187d699e279f0712a8a48dfe1595
zjp <jiping_zhou@foxmail.com>
2026-05-14 19:10:44 +0800

    test: assert stdout.contains(substr)

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,7 +2,7 @@ use expect_test::expect_file;
 use std::{process::Command, sync::LazyLock};
 
 macro_rules! snapshot {
-    ($name:ident $success:literal) => {
+    ($name:ident $success:literal $substr:expr) => {
         #[test]
         fn $name() {
             let dir = stringify!($name);
@@ -13,7 +13,13 @@ macro_rules! snapshot {
                 .unwrap();
 
             let stdout = strip_pwd(std::str::from_utf8(&output.stdout).unwrap());
+            // Check if the expansion expected.
             expect_file![format!("{dir}/cargo_expand.rs")].assert_eq(&stdout);
+
+            // Check if the output contains `pub(in crate::xx::subsystem)`.
+            for substr in (&$substr as &[&str]) {
+                assert!(stdout.contains(substr), "{substr:?} should be included in the output of {dir}");
+            }
 
             // Stdout and stderr are redirected to user.
             let status = Command::new("cargo")
@@ -21,19 +27,24 @@ macro_rules! snapshot {
                 .current_dir(format!("tests/{dir}"))
                 .status()
                 .unwrap();
+            // Check if the code compiles.
             assert_eq!(status.success(), $success);
         }
     };
-    (@fail $($name:ident),+ $(,)?) => {
-        $(snapshot! { $name false })+
+    (@fail $($name:ident [$($substr:literal),*]),+ $(,)?) => {
+        $(snapshot! { $name false [$($substr),*] })+
     };
-    (@success $($name:ident),+ $(,)?) => {
-        $(snapshot! { $name true })+
+    (@success $($name:ident [$($substr:literal),*]),+ $(,)?) => {
+        $(snapshot! { $name true [$($substr),*] })+
     };
 }
 
-snapshot! { @fail err_single_ident }
-snapshot! { @success ok_single_ident }
+snapshot! {
+    @fail err_single_ident ["pub(in crate::procfs)"]
+}
+snapshot! {
+    @success ok_single_ident ["pub(in crate::procfs)", "pub(in crate::fs::procfs)", "pub(in crate::fs2::procfs)"]
+}
 
 // Don't include local path in output.
 fn strip_pwd(s: &str) -> String {
